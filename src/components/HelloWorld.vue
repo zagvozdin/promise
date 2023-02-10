6d9f0b9bb189d1b70d41e9fe312035c58388e06c
Zagvozdin <zzzrus@mail.ru>
2023-02-10 22:05:56 +0700

    Promise example

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,55 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <p>{{ msg }}</p>
+    <button @click="start()">Start</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+
+  methods: {
+    async start() {
+      console.log("Start pressed");
+      console.log(await this.one());
+      console.log(await this.two());
+      console.log(await this.three());
+      console.log(await this.four());
+    },
+    async one() {
+      console.log("insise ONE begin");
+      return new Promise((res) => {
+        setTimeout(() => res(1), 2000);
+      });
+    },
+    async two() {
+      console.log("insise TWO begin");
+      return new Promise((res) => {
+        setTimeout(() => res(2), 2000);
+      });
+    },
+    async three() {
+      console.log("insise THREE begin");
+      return new Promise((res) => {
+        setTimeout(() => res(3), 2000);
+      });
+    },
+
+    async four() {
+      console.log("inside FOUR begin");
+      const p = new Promise((res) => {
+        setTimeout(() => res(4), 2000);
+      });
+      return p.then((res) => {
+        return res;
+      });
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
